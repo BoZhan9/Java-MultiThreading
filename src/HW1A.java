25c285b5bd0561df3c1ad8f5cede0462b7fdd51a
brianchang <brian.pcchang@gmail.com>
2023-01-11 14:37:00 -0800

    Try another method

---
+++ src/HW1A.java
@@ -0,0 +1,24 @@
+public class HW1A {
+    private static volatile boolean flag;
+    public static void main(String[] args) throws InterruptedException {
+        Thread t1 = new Thread(() -> {
+            while (!flag) {
+                System.out.println("start a new thread count numbers");
+                for (int i = 1; i <= 26; i++) {
+                    System.out.println(Thread.currentThread().getName() + " " + i);
+                    flag = true;
+                }}});
+        Thread t2 = new Thread(() -> {
+            while (flag) {
+                System.out.println("start a new thread count alphabets");
+                for (char c = 'a'; c <= 'z'; c++) {
+                    System.out.println(Thread.currentThread().getName() + " " + c);
+                    flag = false;
+                }}});
+
+        t1.start();
+        t2.start();
+        t1.join();
+        t2.join();
+    }
+}
